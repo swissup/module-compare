490044bf05ba26036f795f9daa2c4a3b40a9ecb6
Vitaliy Ohnitskiy <tmvitaly@gmail.com>
2024-03-11 12:19:01 +0200

    Remove compare link from the highlight widgets when the module is disabled.

---
+++ Block/Product/Widget/ProductsList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Swissup\Compare\Block\Product\Widget;
+
+use Magento\Framework\View\Element\Template;
+
+class ProductsList extends Template
+{
+    protected $_template = 'Swissup_Compare::product/widget/productsList.phtml';
+}
